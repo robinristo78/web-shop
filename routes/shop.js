cd86d14de42e558569d156037172c0b5936c6276
robinristo <robin-risto.kivit@voco.ee>
2025-01-16 13:26:31 +0200

    alustatud admin.js ja shop.js

---
+++ routes/shop.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const shopController = require('../controllers/shop');
+const router = express.Router();
+
+router.get('/', shopController.getShop);
+
+module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -1,15 +1,18 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: '192.168.36.100',
     user: 'dbuser',
-    password: 'qwerty',
-    database: 'joga_mysql'
+    password: 'qwerty1234',
+    database: 'webshop'
 });
 
 connection.connect((err) => {
     if(err) throw err;
     console.log('MySQL server is connected');
+    connection.query('select * from products;', (error, results, fields) => {
+        console.log(results);
+    });
 });
 
 module.exports = connection;
