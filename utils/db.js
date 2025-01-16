ee18050fa4cbbdf41a19ad3493b664cfc863825a
robinristo <robin-risto.kivit@voco.ee>
2025-01-16 12:35:11 +0200

    Lisatud db.js

---
+++ utils/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'dbuser',
+    password: 'qwerty',
+    database: 'joga_mysql'
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('MySQL server is connected');
+});
+
+module.exports = connection;
