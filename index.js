a5edc33c52acd20c63e6fa42bdf397e5ba5713f9
robinristo <robin-risto.kivit@voco.ee>
2025-01-16 13:06:46 +0200

    täiendatud index.js 'Vahevara'

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-//test
-//test2
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('This is in the middleware');
+});
+
+app.listen(3033, () => {
+    console.log('server is connected at localhost:3033');
+});
